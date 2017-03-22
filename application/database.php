674a7a6802001b1553d405e3e0388b6d5f26d0ce
caokai <2336526507@qq.com>
2017-03-22 15:12:43 +0800

    1

--- application/database.php
+++ application/database.php
@@ -15,21 +15,21 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'chenyou',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
-    'dsn'             => '',
+    'dsn'             => '8.8.8.8',
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix'          => 'cy_',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
